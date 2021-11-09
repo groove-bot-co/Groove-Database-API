7e8ea75b823eaa46d0651227e8376e028261fb4c
pix <oniichan.pix@gmail.com>
2021-11-09 14:24:54 -0500

    Create index.js

---
+++ database/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+	GuildSchema: require('./guildsettings'),
+	RankSchema: require('./Level'),
+	WarningSchema: require('./warning'),
+	GiveawaySchema: require('./giveaway'),
+	PlaylistSchema: require('./playlist'),
+	ReactionRoleSchema: require('./reactionrole'),
+	timeEventSchema: require('./evvents'),
+	TagsSchema: require('./tag'),
+	userSchema: require('./userSettings.js'),
+};
