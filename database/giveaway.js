0930b94f6ebebd05acb7915602071a87b3b476be
pix <oniichan.pix@gmail.com>
2021-11-09 14:21:32 -0500

    Create giveaway.js

---
+++ database/giveaway.js
@@ -0,0 +1,18 @@
+
+const { Schema, model } = require('mongoose');
+
+const giveawaySchema = Schema({
+	messageID: String,
+	channelID: String,
+	guildID: String,
+	startAt: Number,
+	endAt: Number,
+	ended: Boolean,
+	winnerCount: Number,
+	winners: Array,
+	prize: String,
+	hostedBy: String,
+	messages: Schema.Types.Mixed,
+});
+
+module.exports = model('Giveaway', giveawaySchema);
