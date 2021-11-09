19049de73150ac2a853d931d8ac75bbeed21eea4
tk <93726904+tkisthebesttt@users.noreply.github.com>
2021-11-09 15:05:20 -0600

    Update usersettings.js

--- database/usersettings.js
+++ database/usersettings.js
@@ -1,12 +1,12 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = Schema({
-	userID: String,
-	premium: { type: Boolean, default: false }, 
-	premiumSince: String, 
-	rankImage: Schema.Types.Buffer, 
-	Language: { type: String, default: 'en-US' },
-	cmdBanned: { type: Boolean, default: false }, 
+    userID: String,
+    premium: { type: Boolean, default: false }, //premuim users for groove
+    premiumSince: String, // premium-only - custom rank background
+    rankImage: Schema.Types.Buffer, // Will be used for the website (or DM's)
+    Language: { type: String, default: 'en-US' },
+    cmdBanned: { type: Boolean, default: false }, // If the user is banned from using commands or not
 });
 
-module.exports = model('Users', userSchema);
+module.exports = model('Users', userSchema); 
